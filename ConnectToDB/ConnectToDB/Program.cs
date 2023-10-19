bdd431b65d9e147e6d9075eac244edaa533001f9
ktlhape <ktlhape@iie.ac.za>
2023-10-19 16:00:58 +0200

    Insert data functionality from a model

--- ConnectToDB/ConnectToDB/Program.cs
+++ ConnectToDB/ConnectToDB/Program.cs
@@ -11,7 +11,7 @@ namespace ConnectToDB
         static void Main(string[] args)
         {
             //Delete data
-            Employee.DeleteData("EM16330");
+            //Employee.DeleteData("EM16330");
             //Add Data
             Employee em = new Employee("0000000", "Serena", "Williams", 20000);
             em.InsertData();
